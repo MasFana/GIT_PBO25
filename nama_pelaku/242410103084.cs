274d7498efc9008e41bb432ad3eb07ee3d4b3e1d
Mita <242410103084@mail.unej.ac.id>
2025-04-02 20:10:28 +0700

    aku nambah NIM 3084

---
+++ nama_pelaku/242410103084.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_PBO25.nama_pelaku
+{
+    class _242410103084
+    {
+    }
+}
